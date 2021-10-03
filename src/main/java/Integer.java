0a8e0c08bee849306859509113e278d0309ba6e8
= <kyle.hobeck@mail.utoronto.ca>
2021-10-02 23:45:29 -0400

    added more integer stuff

--- src/main/java/Integer.java
+++ src/main/java/Integer.java
@@ -7,4 +7,12 @@ public class Integer {
     private int get_value(){
         return this.value;
     }
+    private Integer add_integer(Integer other){
+        Integer new_integer = new Integer();
+        new_integer.set_value(this.value +other.get_value());
+        return new_integer;
+    }
+    public String print(){
+        return this.value + "";
+    }
 }
