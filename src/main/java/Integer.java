e74c76779c60bec0981a8a0f2416bf22d7e5f7a1
= <kyle.hobeck@mail.utoronto.ca>
2021-10-02 23:41:38 -0400

    added integer stuff

--- src/main/java/Integer.java
+++ src/main/java/Integer.java
@@ -1,5 +1,10 @@
 public class Integer {
     private int value;
 
-
+    private void set_value(int val){
+        this.value = val;
+    }
+    private int get_value(){
+        return this.value;
+    }
 }
