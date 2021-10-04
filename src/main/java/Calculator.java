cffd348752debc0447f4f3a80e1aa77176f6ad57
= <kyle.hobeck@mail.utoronto.ca>
2021-10-04 12:49:27 -0400

    redid things

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static void main(String[] args){
         IntegerValue n4 = new IntegerValue(3);
         IntegerValue n5 = new IntegerValue(-12);
-        Fraction n6 = new Fraction(new IntegerValue(1), new IntegerValue(4));
-        RealNumber n7 = n4.divide_integer(n5);
+        Fraction n6 = new Fraction(new IntegerValue(10), new IntegerValue(1020));
+        n6.simplify_fraction().print();
     }
 }
