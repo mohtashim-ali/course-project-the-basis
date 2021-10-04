b3b6e1318118f95c2b1ba30d9234e71d43ba03b4
= <kyle.hobeck@mail.utoronto.ca>
2021-10-04 13:04:00 -0400

    Added Complex Number Class

--- src/main/java/ComplexNumber.java
+++ src/main/java/ComplexNumber.java
@@ -1,2 +1,23 @@
 public class ComplexNumber {
+    private RealNumber real_part;
+    private RealNumber imaginary_part;
+    public ComplexNumber(RealNumber a, RealNumber b){
+        this.real_part = a;
+        this.imaginary_part = b;
+    }
+
+    public ComplexNumber add(ComplexNumber other){
+        // pass
+    }
+    public ComplexNumber subtract(ComplexNumber other){
+        // pass
+    }
+    public ComplexNumber multiply(ComplexNumber other){
+        // pass
+    }
+    public ComplexNumber divide(ComplexNumber other){
+        // pass
+    }
+    // Idea: All other classes inherit these four methods since every number is in the complex field just change how they
+    // operate.
 }
