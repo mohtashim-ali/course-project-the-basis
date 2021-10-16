c98e687c5187fa758c2a00d290131a4a995cca8b
= <hamza.nur@mail.utoronto.ca>
2021-10-15 20:54:25 -0400

    added multiplication and division tests!

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,18 @@ public class CalculatorTest {
         Expression temp = input.processInput();
         assert temp.compute() == 1.0;
     }
-
+    @Test
+    public void multiplication() {
+        InputProcessor input = new InputProcessor("5*4");
+        Expression temp = input.processInput();
+        assert temp.compute() == 20.0;
+    }
+    @Test
+    public void division() {
+        InputProcessor input = new InputProcessor("10/5");
+        Expression temp = input.processInput();
+        assert temp.compute() == 2.0;
+    }
 
 
 }
