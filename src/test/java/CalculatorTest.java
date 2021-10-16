3f5d610f63cb6d9d98caecb940f53aef4a02cdc2
Mohtashim Ali <mohtashim.ali@mail.utoronto.ca>
2021-10-15 20:39:00 -0400

    Created test case file and started test cases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void addition() {
+        WebProcessor input = new WebProcessor("5-4");
+        Expression temp = input.processInput();
+        assert temp.compute() == 1.0;
+
+    }
+
+
 }
