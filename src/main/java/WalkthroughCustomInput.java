623265847b1de3c24668e165643d8c5eadd7abac
Achraf Boufennara <achraf.boufennara@mail.utoronto.ca>
2021-10-16 17:10:38 -0400

    Update WalkthroughCustomInput.java

--- src/main/java/WalkthroughCustomInput.java
+++ src/main/java/WalkthroughCustomInput.java
@@ -4,39 +4,30 @@ import java.util.*;
 public class WalkthroughCustomInput{
 
 
-    public void main(String[] args) throws ExpressionException {
+    public static void main(String[] args) throws ExpressionException {
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please type in a mathematical expression:");
         String expression = input.nextLine();
 
-        InputProcessor user = new InputProcessor(expression);
+        StringBuffer s = new StringBuffer();
+        String[] arr = new String[expression.length()+expression.length()-1];
+        if (!expression.contains(" ")) {
+            for (int i=0; i<expression.length(); i++) {
+                arr[i] = String.valueOf(expression.charAt(i));
+                arr[i+1] = " ";
+            }
+
+            for(int i = 0; i < arr.length-expression.length()+1; i++) {
+                s.append(arr[i]);
+                s.append(" ");
+            }
+        }
+        InputProcessor user = new InputProcessor(s.toString());
         Expression userinput = user.processInput(user.listInput());
 
         System.out.println(userinput.compute());
-
-        /**
-         * Checks if the given input includes whitespaces and adds whitespaces if none are present.
-         */
-        public String validateInput(String expression) {
-            StringBuffer s = new StringBuffer();
-            String[] arr = new String[expression.length()+expression.length()-1];
-            if (!expression.contains(" ")) {
-                for (int i=0; i<expression.length(); i++) {
-                    arr[i] = String.valueOf(expression.charAt(i));
-                    arr[i+1] = " ";
-                }
-
-                for(int i = 0; i < arr.length-expression.length()+1; i++) {
-                    s.append(arr[i]);
-                    s.append(" ");
-                }
-            }
-            System.out.println(s);
-        }
-
-
 
     }
 
