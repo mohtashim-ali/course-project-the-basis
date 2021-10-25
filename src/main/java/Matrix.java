beff3d9e20bc45ba5048e9e12678941c171357b1
Achraf Boufennara <achraf.boufennara@mail.utoronto.ca>
2021-10-25 12:38:49 -0400

    Matrix Class

---
+++ src/main/java/Matrix.java
@@ -0,0 +1,15 @@
+public class Matrix extends MatrixHandler{
+    /**
+     Creates a matrix.
+     */
+
+    int[][] matrix;
+
+    /**
+     *
+     * @param matrix int[][]
+     */
+    public Matrix(int[][] matrix) {
+        super(matrix);
+    }
+}
---
+++ src/main/java/MatrixHandler.java
@@ -0,0 +1,56 @@
+public class MatrixHandler {
+    /**
+     Creates a matrix.
+     */
+
+    int[][] matrix;
+
+    /**
+     *
+     * @param matrix int[][]
+     */
+    public MatrixHandler(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    /**
+     * Adds 2 matrices
+     * @param other
+     * @return
+     */
+    public Matrix add_matrix(Matrix other) {
+        // todo
+        return null;
+    }
+
+    /**
+     * Substracts 2 matrices
+     * @param other
+     * @return
+     */
+    public Matrix substract_matrix(Matrix other) {
+        // todo
+        return null;
+    }
+
+    /**
+     * Returns the RREF of this matrix.
+     * @return
+     */
+    public Matrix RREF() {
+        // todo
+        return null;
+    }
+
+    /**
+     * Returns the inverse of this matrix.
+     * @param other
+     * @return
+     */
+    public Matrix inverse() {
+        // todo
+        return null;
+    }
+
+}
+
