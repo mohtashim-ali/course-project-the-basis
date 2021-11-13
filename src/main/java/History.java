15c4730bc40867f6ff89ce30f7de2409456c9da2
Achraf Boufennara <achraf.boufennara@mail.utoronto.ca>
2021-11-13 14:24:12 -0500

    Create History.java

---
+++ src/main/java/History.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class History {
+    /**
+     * Stores all previous inputs/calculations.
+     */
+    public ArrayList<String> data;
+
+    public History() {
+        ArrayList
+    }
+}
