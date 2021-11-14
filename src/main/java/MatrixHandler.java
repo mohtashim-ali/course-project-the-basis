4e6ea0e50bb384867831b253cce8d106f1bd2620
= <hamza.nur@mail.utoronto.ca>
2021-11-14 01:56:13 -0500

    added RREF method and tests

--- src/main/java/MatrixHandler.java
+++ src/main/java/MatrixHandler.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MatrixHandler {
     /**
@@ -38,75 +37,137 @@ public class MatrixHandler {
     }
 
     /**
-     * Multiples the matrix by a scalar.
-     *
-     * @param scalar
-     */
-    public void scalarMult(double scalar) {
-        for (int i = 0; i < this.matrix.size(); i++) {
-            for (int j = 0; j < this.matrix.get(i).size(); j++) {
-                double temp = scalar * this.matrix.get(i).get(j);
-                this.matrix.get(i).set(j, temp);
-            }
-        }
-    }
-
-    /**
      * Adds row2 to row1.
-     *
      * @param row1
      * @param row2
      * @return
      */
-    public void rowAdd(int row1, int row2) {
-        for (int i = 0; i < this.matrix.get(row1).size(); i++) {
-            this.matrix.get(row1 - 1).set(i, this.matrix.get(row2 - 1).get(i));
+    public ArrayList<Double> rowAdd(ArrayList<Double> row1, ArrayList<Double> row2) {
+        ArrayList<Double> new_row = new ArrayList<>();
+        for(int i = 0; i < row1.size(); i++) {
+            new_row.add(row1.get(i) + row2.get(i));
         }
+        return new_row;
     }
 
     /**
      * Adds scalar * row2 to row1.
-     *
-     * @param row1
-     * @param row2
+     * @param indexRow1
+     * @param indexRow2
      * @param scalar
      * @return
      */
-    public void addScalarMult(int row1, int row2, double scalar) {
-        for (int i = 0; i < this.matrix.get(row1).size(); i++) {
-            this.matrix.get(row1 - 1).set(i, scalar * this.matrix.get(row2 - 1).get(i));
+    public ArrayList<ArrayList<Double>> addScalarMult(int indexRow1, int indexRow2, double scalar) {
+        for(int i = 0; i < this.matrix.get(indexRow1).size(); i++) {
+            this.matrix.get(indexRow1).set(i, scalar * this.matrix.get(indexRow2).get(i));
         }
+        return this.matrix;
     }
 
     /**
      * Swaps 2 rows in a given matrix.
-     *
-     * @param row1
-     * @param row2
+     * @param indexRow1
+     * @param indexRow2
      * @return
      */
-    public void rowSwap(int row1, int row2) {
-        ArrayList<Double> temp1 = this.matrix.get(row1 - 1);
-        ArrayList<Double> temp2 = this.matrix.get(row2 - 1);
-        this.matrix.set(row1 - 1, temp2);
-        this.matrix.set(row2 - 1, temp1);
+    public ArrayList<ArrayList<Double>> rowSwap(int indexRow1, int indexRow2) {
+        ArrayList<Double> temp1 = this.matrix.get(indexRow1);
+        ArrayList<Double> temp2 = this.matrix.get(indexRow2);
+        this.matrix.set(indexRow1, temp2);
+        this.matrix.set(indexRow2, temp1);
+        return this.matrix;
     }
 
-    /**
-     * Returns the RREF of this matrix.
-     *
-     * @return
-     */
-    public Matrix RREF() {
-        return null;
+    public ArrayList<Double> scalMult(ArrayList<Double> row, double scalar) {
+        ArrayList<Double> new_row = new ArrayList<>();
+        for (int i=0; i<row.size(); i++) {
+            new_row.add(i, row.get(i)*scalar);
+        }
+        return new_row;
     }
 
     /**
      * Returns the inverse of this matrix.
-     *
+     * @param row
      * @return
      */
-    public Matrix inverse() {
+    public boolean rowOfZeros(ArrayList<Double> row) {
+        for (int i=0; i < row.size(); i++) {
+            if (row.get(i) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void moveZerosToBottom(int index) {
+        int curr_row = index;
+        for (int j = 0; j < this.matrix.size() - index - 1; j++) {
+            rowSwap(curr_row, curr_row + 1);
+            curr_row += 1;
+        }
+    }
+
+    public boolean zeroMatrix() {
+        for (ArrayList<Double> doubles : this.matrix) {
+            if (!rowOfZeros(doubles)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns the RREF of this matrix.
+     * @return
+     */
+    public ArrayList<ArrayList<Double>> RREF() {
+
+        if (zeroMatrix()) {
+            return this.matrix;
+        }
+
+        for (int i=0; i < this.matrix.size(); i++) {
+            if (rowOfZeros(this.matrix.get(i))) {
+                moveZerosToBottom(i);
+            }
+        }
+        for (int k=0; k<this.matrix.size(); k++) {
+            ArrayList<Double> curr_row = this.matrix.get(k);
+            if (!rowOfZeros(curr_row)) {
+                Double value = curr_row.get(k);
+                curr_row = scalMult(curr_row, (1/value));
+                this.matrix.set(k, curr_row);
+                for (int j=0; j < this.matrix.size(); j++) {
+                    if (j != k) {
+                        ArrayList<Double> added_row;
+                        ArrayList<Double> other_row = this.matrix.get(j);
+                        double multiple = other_row.get(k);
+                        ArrayList<Double> scaleRow = scalMult(curr_row, (multiple) * -1);
+                        added_row = rowAdd(other_row, scaleRow);
+                        this.matrix.set(j, added_row);
+                    }
+                }
+            }
+        }
+        for (ArrayList<Double> doubles : this.matrix) {
+            for (int m = 0; m < doubles.size(); m++) {
+                if (doubles.get(m) == -0.0) {
+                    doubles.set(m, 0.0);
+                }
+            }
+        }
+
+
+        return this.matrix;
+    }
+
+
+    /**
+     * Returns the inverse of this matrix.
+     * @return
+     */
+    public double [][] inverse() {
         // todo
         return null;
     }
@@ -114,9 +175,9 @@ public class MatrixHandler {
     @Override
     public String toString() {
         StringBuilder final_string = new StringBuilder("[[");
-        for (int i = 0; i < this.matrix.size(); i++) {
-            for (int j = 0; j < this.matrix.get(i).size(); j++) {
-                if (j != this.matrix.get(i).size() - 1) {
+        for(int i = 0; i < this.matrix.size(); i++) {
+            for(int j = 0; j < this.matrix.get(i).size(); j++) {
+                if(j != this.matrix.get(i).size() - 1) {
                     final_string.append(this.matrix.get(i).get(j)).append(", ");
                 } else {
                     final_string.append(this.matrix.get(i).get(j)).append("],").append("\n").append("[");
@@ -125,6 +186,7 @@ public class MatrixHandler {
         }
         return final_string.toString().substring(0, final_string.length() - 3) + "]";
     }
+
 
     public double determinant() {
         if (this.matrix.size() == 2 && this.matrix.get(0).size() == 2) {
--- src/test/java/MatrixHandlerTest.java
+++ src/test/java/MatrixHandlerTest.java
@@ -1,6 +1,7 @@
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MatrixHandlerTest extends TestCase {
 
@@ -127,5 +128,124 @@ public class MatrixHandlerTest extends TestCase {
         assert temp.determinant() == 472;
     }
 
+//    public void zeroMatrix() {
+//        ArrayList<Double> m1 = new ArrayList<>() {{
+//            add(0.0);
+//            add(0.0);
+//            add(0.0);
+//        }};
+//        ArrayList<Double> m2 = new ArrayList<>() {{
+//            add(0.0);
+//            add(0.0);
+//            add(0.0);
+//        }};
+//        ArrayList<Double> m3 = new ArrayList<>() {{
+//            add(0.0);
+//            add(0.0);
+//            add(0.0);
+//        }};
+//        ArrayList<ArrayList<Double>> new_matrix = new ArrayList<>() {{
+//            add(m1);
+//            add(m2);
+//            add(m3);
+//        }};
+//        Matrix temp = new Matrix(new_matrix);
+//        assert Objects.equals(temp.RREF().toString(), "[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]");
+//    }
+//
+//    public void testRREF1() {
+//        ArrayList<Double> m1 = new ArrayList<>() {{
+//            add(1.0);
+//            add(3.0);
+//            add(1.0);
+//        }};
+//        ArrayList<Double> m2 = new ArrayList<>() {{
+//            add(-1.0);
+//            add(-2.0);
+//            add(1.0);
+//        }};
+//        ArrayList<Double> m3 = new ArrayList<>() {{
+//            add(3.0);
+//            add(7.0);
+//            add(-1.0);
+//        }};
+//        ArrayList<ArrayList<Double>> new_matrix = new ArrayList<>() {{
+//            add(m1);
+//            add(m2);
+//            add(m3);
+//        }};
+//        Matrix temp = new Matrix(new_matrix);
+//        assert Objects.equals(temp.RREF().toString(), "[[1.0, 0.0, -5.0], [0.0, 1.0, 2.0], [0.0, 0.0, 0.0]]");
+//    }
+//
+//    public void testRREF2() {
+//        ArrayList<Double> m1 = new ArrayList<>() {{
+//            add(1.0);
+//            add(1.0);
+//            add(2.0);
+//        }};
+//        ArrayList<Double> m2 = new ArrayList<>() {{
+//            add(3.0);
+//            add(-1.0);
+//            add(1.0);
+//        }};
+//        ArrayList<Double> m3 = new ArrayList<>() {{
+//            add(-1.0);
+//            add(3.0);
+//            add(4.0);
+//        }};
+//        ArrayList<ArrayList<Double>> new_matrix = new ArrayList<>() {{
+//            add(m1);
+//            add(m2);
+//            add(m3);
+//        }};
+//        Matrix temp = new Matrix(new_matrix);
+//        assert Objects.equals(temp.RREF().toString(), "[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]");
+//    }
 
+    public void testRREF3() {
+        ArrayList<Double> m1 = new ArrayList<>() {{
+            add(3.0);
+            add(8.0);
+            add(-3.0);
+            add(-14.0);
+        }};
+        ArrayList<Double> m2 = new ArrayList<>() {{
+            add(2.0);
+            add(3.0);
+            add(-1.0);
+            add(-2.0);
+        }};
+        ArrayList<Double> m3 = new ArrayList<>() {{
+            add(1.0);
+            add(-2.0);
+            add(1.0);
+            add(10.0);
+        }};
+        ArrayList<Double> m4 = new ArrayList<>() {{
+            add(1.0);
+            add(5.0);
+            add(-2.0);
+            add(-12.0);
+        }};
+        ArrayList<ArrayList<Double>> new_matrix = new ArrayList<>() {{
+            add(m1);
+            add(m2);
+            add(m3);
+            add(m4);
+        }};
+        Matrix temp = new Matrix(new_matrix);
+        System.out.println(temp.RREF().toString());
+        assert Objects.equals(temp.RREF().toString(), "[[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]");
+
+    }
+
+    public void testMoveZerosToBottom() {
+    }
+
+    public void testZeroMatrix() {
+    }
+
+    public void testRREF() {
+    }
 }
