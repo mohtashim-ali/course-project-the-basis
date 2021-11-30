89157750828741192367c97b05d72cf4482824ec
Mohtashim Ali <mohtashim.ali@mail.utoronto.ca>
2021-11-30 14:01:05 -0500

    Almost finished userlog in and signup. Working on validating the input password.

--- src/main/java/UI/CalculatorUI.java
+++ src/main/java/UI/CalculatorUI.java
@@ -1,9 +1,20 @@
 package UI;
+import Controller.CalculatorController;
+
+import java.io.IOException;
+import java.util.*;
 
 public class CalculatorUI {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-
+        Scanner read = new Scanner(System.in);
+        System.out.println("1. Log In");
+        System.out.println("2. Sign Up");
+        int log = read.nextInt();
+        CalculatorController control = new CalculatorController();
+        control.setChoice(log);
+        control.handleUser();
+        }
     }
-}
+
