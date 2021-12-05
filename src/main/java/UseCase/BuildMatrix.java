dc23aee5d12fd2c07099ac8fe566c2a1a604da92
Mohtashim Ali <mohtashim.ali@mail.utoronto.ca>
2021-12-05 17:10:41 -0500

    User can type in integers into matrix as whole numbers now and not forced to type in fractions

--- src/main/java/UseCase/BuildMatrix.java
+++ src/main/java/UseCase/BuildMatrix.java
@@ -9,10 +9,11 @@ public class BuildMatrix {
 
     /**
      * This method created a matrix with the help of the helper method
+     *
      * @return Matrix
      */
 
-    public Matrix createMatrix(){
+    public Matrix createMatrix() {
 
 
         Scanner input = new Scanner(System.in);
@@ -29,39 +30,42 @@ public class BuildMatrix {
     /**
      * This method takes in the users matrix split, reads the matrix and places
      * it into a nested ArrayList which will be used to create a matrix.
+     *
      * @param matrix - Contains the input matrix split into an array
-     * @return ArrayList<ArrayList<Fraction>> - returns the input for Matrix entity
+     * @return ArrayList<ArrayList < Fraction>> - returns the input for Matrix entity
      */
-    public ArrayList<ArrayList<Fraction>> helper(String[] matrix){
+    public ArrayList<ArrayList<Fraction>> helper(String[] matrix) {
 
-        int x  = 1;
+        int x = 1;
         ArrayList<ArrayList<Fraction>> matri = new ArrayList<>();
-        while (x < matrix.length){
-            if (Objects.equals(matrix[x], "[")){
+        while (x < matrix.length) {
+            if (Objects.equals(matrix[x], "[")) {
                 x++;
                 ArrayList<Fraction> inner = new ArrayList<>();
-                while (!matrix[x].equals("]")){
-                    if (!matrix[x].equals(",") && !matrix[x].equals("]")){
-                        int first = Integer.parseInt(matrix[x]);
-                        int second = Integer.parseInt(matrix[x+2]);
-                        Fraction test = new Fraction(first, second);
-                        inner.add(test);
-                        x+= 3;
-                    }
-                    else{
+                while (!matrix[x].equals("]")) {
+                    if (!matrix[x].equals(",") && !matrix[x].equals("]")) {
+                        if ((matrix[x + 1].equals(","))) {
+                            int first = Integer.parseInt(matrix[x]);
+                            Fraction test = new Fraction(first, 1);
+                            inner.add(test);
+                            x += 1;
+
+                        } else {
+                            int first = Integer.parseInt(matrix[x]);
+                            int second = Integer.parseInt(matrix[x + 2]);
+                            Fraction test = new Fraction(first, second);
+                            inner.add(test);
+                            x += 3;
+                        }
+                        matri.add(inner);
+                    } else {
                         x++;
                     }
-
                 }
-                matri.add(inner);
+                return matri;
 
-            }
-            else{
-                x++;
             }
         }
         return matri;
-
     }
-
 }
