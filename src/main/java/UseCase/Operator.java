91aca2d6e8f56f25acc563c8a91922ffd39d3953
= <kyle.hobeck@mail.utoronto.ca>
2021-12-06 00:38:09 -0500

    finalized Parser class

--- src/main/java/UseCase/Operator.java
+++ src/main/java/UseCase/Operator.java
@@ -41,21 +41,43 @@ public class Operator {
         this.operator = operator;
     }
 
-//    public Object resultant() throws ExpressionException {
-//        HashMap<String, Object> hashMap = new HashMap<>()
-//        {{
-//            put("+", new Addition());
-//            put("-", new Subtraction());
-//            put("*", new Multiplication());
-//            put("/", new Division());
-//            put("^", new Exponent());
-//        }};
-//        Calculator context = new Calculator();
-//        for (String key : hashMap.keySet()) {
-//            if (Objects.equals(this.operator, key)) {
-//                context.setStrategy((Computation) hashMap.get(key));
-//            }
-//        }
-//        return context.getStrategy(this.operand1, this.operand2);
-//    }
+    public Object compute() throws ExpressionException {
+        if (!(this.operand1 instanceof Operator) && !(this.operand2 instanceof Operator)){
+            Operator op = new Operator(this.operand1, this.operator, this.operand2);
+            return op.resultant();
+        }
+        if (this.operand1 instanceof Operator && !(this.operand2 instanceof Operator)){
+            Operator op = new Operator(((Operator) this.operand1).compute(), this.operator, this.operand2);
+            return op.resultant();
+        }
+        if (!(this.operand1 instanceof Operator)){
+            Operator op = new Operator(this.operand1, this.operator, ((Operator) this.operand2).compute());
+            return op.resultant();
+        }
+        else{
+            Object left = ((Operator) this.operand1).compute();
+            Object right = ((Operator) this.operand2).compute();
+            return new Operator(left, this.operator, right).resultant();
+        }
+
+
+    }
+
+    public Object resultant() throws ExpressionException {
+        HashMap<String, Object> hashMap = new HashMap<>()
+        {{
+            put("+", new Addition());
+            put("-", new Subtraction());
+            put("*", new Multiplication());
+            put("/", new Division());
+            put("^", new Exponent());
+        }};
+        Calculator context = new Calculator();
+        for (String key : hashMap.keySet()) {
+            if (Objects.equals(this.operator, key)) {
+                context.setStrategy((Computation) hashMap.get(key));
+            }
+        }
+        return context.getStrategy((double) this.operand1, (double) this.operand2);
+    }
 }
