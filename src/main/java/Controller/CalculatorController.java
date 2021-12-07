ce7318809de0aa9ef73b3e28b378ffdf1cfd6d66
Mohtashim Ali <mohtashim.ali@mail.utoronto.ca>
2021-12-07 17:53:02 -0500

    Fixed SRP in controllers, and caught errors. Will check CA with exceptions

--- src/main/java/Controller/CalculatorController.java
+++ src/main/java/Controller/CalculatorController.java
@@ -14,70 +14,100 @@ import java.util.Scanner;
 
 public class CalculatorController {
 
-    private int choice;
-    private final CurrentUser curr = new CurrentUser();
-    private final UserHistory userHistory = new UserHistory();
+    private UserHistory userHistory;
 
 
-    public void setChoice(int choice) {
-        this.choice = choice;
-    }
 
+//    public boolean handleUser() {
+//            if (choice == 1) {
+//                UserLogIn login = new UserLogIn();
+//                try {
+//                    curr.setCurrentUser(login.logIn());
+//                } catch (IOException | ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                userHistory.setCurr(curr);
+//                System.out.println("You have successfully logged in!");
+//                return true;
+//            } else if (choice == 2) {
+//                UserSignUp signup = new UserSignUp();
+//                try {
+//                    curr.setCurrentUser(signup.signUp());
+//                } catch (IOException | ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                userHistory.setCurr(curr);
+//                System.out.println("You have successfully signed up!");
+//                return true;
+//            }
+//            else{
+//                System.out.println("Invalid Option, Please Try Again!");
+//                Scanner input = new Scanner(System.in);
+//                choice = input.nextInt();
+//                return handleUser();
+//            }
+//    }
 
-    public boolean handleUser() throws IOException, ClassNotFoundException {
-            if (choice == 1) {
-                UserLogIn login = new UserLogIn();
-                curr.setCurrentUser(login.logIn());
-                userHistory.setCurr(curr);
-                System.out.println("You have successfully logged in!");
-                return true;
-            } else if (choice == 2) {
-                UserSignUp signup = new UserSignUp();
-                curr.setCurrentUser(signup.signUp());
-                userHistory.setCurr(curr);
-                System.out.println("You have successfully signed up!");
-                return true;
-            }
-            else{
-                System.out.println("Invalid Option, Please Try Again!");
-                Scanner input = new Scanner(System.in);
-                choice = input.nextInt();
-                return handleUser();
-            }
-    }
-
-    public void handleOperations() throws IOException, ParserException, ExpressionException, ClassNotFoundException {
-        if(this.choice == 1) {
+    public void handleOperations(){
+        System.out.println("1. Compute");
+        System.out.println("2. Matrix");
+        System.out.println("3. My History");
+        System.out.println("4. Switch Accounts");
+        System.out.println("5. Power Off");
+        Scanner math = new Scanner(System.in);
+        int choice = math.nextInt();
+        if(choice == 1) {
             Date date = new Date();
             SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
             BuildExpression buildExpression = new BuildExpression();
             StringBuilder fixed_input = buildExpression.createExpression();
             userHistory.addToHistory(fixed_input.toString(), formatter.format(date)); // Temporary time
             Parser p = new Parser(fixed_input.toString());
-            System.out.println(p.stringToExpression().compute());
-        } else if (this.choice == 2) {
+            try {
+                System.out.println(p.stringToExpression().compute());
+                handleOperations();
+            } catch (ParserException e){
+                System.out.println("Invalid operator, please try again!");
+                handleOperations();
+            }catch (ExpressionException ex){
+                System.out.println("Invalid Expression, please try again!");
+                handleOperations();
+            }
+        } else if (choice == 2) {
             Date date = new Date();
             SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
             ComputeMatrix computeMatrix = new ComputeMatrix();
             userHistory.addToHistory(String.valueOf(computeMatrix.computeMatrix()), formatter.format(date));
-        } else if (this.choice == 3) {
-            userHistory.readFromHistory();
+            handleOperations();
+        } else if (choice == 3) {
+            try{
+                userHistory.readFromHistory();
+                handleOperations();
+            } catch (IOException e){
+                System.out.println("Could not read from file");
+                handleOperations();
+            }
+
         }
-        else if (this.choice == 4){
-            choice = 1;
+        else if (choice == 4){
             System.out.println("You have been successfully logged out!");
             System.out.println("Log-In Menu");
-            handleUser();
+            UserController n = new UserController();
+            n.setChoice(1);
+            n.handleUser();
+            handleOperations();
         }
-        else if (this.choice == 5){
+        else if (choice == 5){
             System.out.println("Thank you for using The Basis Calculator!");
             System.exit(0);
         }
         else{
             System.out.println("Invalid Option, Please Try Again!");
-            Scanner input = new Scanner(System.in);
-            choice = input.nextInt();
             handleOperations();
         }
+    }
+
+    public void setUserHistory(UserHistory userHistory) {
+        this.userHistory = userHistory;
     }
 }
--- src/main/java/UI/CalculatorUI.java
+++ src/main/java/UI/CalculatorUI.java
@@ -1,44 +1,40 @@
 package UI;
 import Controller.CalculatorController;
-import Entity.ExpressionException;
-import UseCase.BasicOperations.ParserException;
+import Controller.UserController;
 
-import java.io.IOException;
 import java.util.*;
 
 public class CalculatorUI {
 
-    public static boolean quit = false;
-    public CalculatorController control;
-    public void setControl(CalculatorController control){
-        this.control = control;
-    }
+//    public static boolean quit = false;
+    public static UserController control = new UserController();
 
-    public void main() throws IOException, ClassNotFoundException, ExpressionException, ParserException {
+    public static void main() {
 
         Scanner read = new Scanner(System.in);
         System.out.println("1. Log In");
         System.out.println("2. Sign Up");
         int log = read.nextInt();
         control.setChoice(log);
-        if (control.handleUser()) {
-            do {
-                System.out.println("1. Compute");
-                System.out.println("2. Matrix");
-                System.out.println("3. My History");
-                System.out.println("4. Switch Accounts");
-                System.out.println("5. Power Off");
-                Scanner math = new Scanner(System.in);
-                int choice = math.nextInt();
-                control.setChoice(choice);
-                control.handleOperations();
-            } while (!quit); // allows the menu options to keep on running
-        }
-        else{
-            System.out.println("Invalid input, please try again!");
-            main();
-        }
+        control.handleUser();
+//        if (control.handleUser()) {
+//            do {
+//                System.out.println("1. Compute");
+//                System.out.println("2. Matrix");
+//                System.out.println("3. My History");
+//                System.out.println("4. Switch Accounts");
+//                System.out.println("5. Power Off");
+//                Scanner math = new Scanner(System.in);
+//                int choice = math.nextInt();
+//                control.setChoice(choice);
+////                control.handleOperations();
+//            } while (!quit); // allows the menu options to keep on running
+//        }
+//        else{
+//            System.out.println("Invalid input, please try again!");
+//            main();
+//        }
+//    }
     }
-
 }
 
