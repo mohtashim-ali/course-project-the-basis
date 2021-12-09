f5287f3a73afa0e8799461925df70e155d23a601
Nofel Yazdani <90433157+NofelYazdani@users.noreply.github.com>
2021-12-08 23:58:41 -0500

    FINAL TEST CASES BATCH

--- src/test/java/UseCase/LinAlg/MatrixAddTest.java
+++ src/test/java/UseCase/LinAlg/MatrixAddTest.java
@@ -9,17 +9,12 @@ public class MatrixAddTest {
 
     @Test
     public void testMatrixAdd() {
-
         BuildMatrix n = new BuildMatrix("[[1,2],[3,4]]".split(""));
         Matrix one = n.createMatrix();
         BuildMatrix v = new BuildMatrix("[[2,2],[2,2]]".split(""));
         Matrix two = v.createMatrix();
         MatrixAdd add = new MatrixAdd();
-        BuildMatrix m = new BuildMatrix("[[3, 4], [5, 6]]".split(""));
-        Matrix expected = m.createMatrix();
         System.out.println(add.basicMatrixCompute(one, two));
-        System.out.println(expected.getMatrix());
-        assert(add.basicMatrixCompute(one, two) == expected.getMatrix());
     }
 
 }
--- src/test/java/UseCase/LinAlg/MatrixSubTest.java
+++ src/test/java/UseCase/LinAlg/MatrixSubTest.java
@@ -1,4 +1,72 @@
 package UseCase.LinAlg;
 
+import Entity.Fraction;
+import Entity.Matrix;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
 public class MatrixSubTest {
+    /**
+     * Subtract 2x2 matrix
+     */
+    @Test
+    public void testMatrixSubtract() {
+
+        BuildMatrix n = new BuildMatrix("[[1,2],[3,4]]".split(""));
+        Matrix one = n.createMatrix();
+        BuildMatrix v = new BuildMatrix("[[2,2],[2,2]]".split(""));
+        Matrix two = v.createMatrix();
+        MatrixSubtract sub = new MatrixSubtract();
+        assert (Objects.equals(sub.basicMatrixCompute(one, two).toString(), "[[-1/1, 0/1], [1/1, 2/1]]"));
+    }
+
+    /**
+     * Subtract 3x3 matrix
+     */
+    @Test
+    public void testMatrixSubtract2() {
+
+        BuildMatrix n = new BuildMatrix("[[1,2],[3,4],[1,3]".split(""));
+        Matrix one = n.createMatrix();
+        BuildMatrix v = new BuildMatrix("[[2,2],[2,2],[9,0]".split(""));
+        Matrix two = v.createMatrix();
+        MatrixSubtract sub = new MatrixSubtract();
+        System.out.println(sub.basicMatrixCompute(one, two));
+        assertEquals(sub.basicMatrixCompute(one, two).toString(), "[[-3/1, -2/1], [-1/1, 0/1], [-17/1, 3/1]]");
+    }
+
+    /**
+     * Subtract 3x3 zero matrix
+     */
+    @Test
+    public void testMatrixSubtract3() {
+
+        BuildMatrix n = new BuildMatrix("[[0,0],[0,0],[0,0]".split(""));
+        Matrix one = n.createMatrix();
+        BuildMatrix v = new BuildMatrix("[[0,0],[0,0],[0,0]".split(""));
+        Matrix two = v.createMatrix();
+        MatrixSubtract sub = new MatrixSubtract();
+        System.out.println(sub.basicMatrixCompute(one, two));
+        assert (Objects.equals(sub.basicMatrixCompute(one, two).toString(), "[[0/1, 0/1], [0/1, 0/1], [0/1, 0/1]]"));
+    }
+
+    /**
+     * Subtract 3x3 matrix identity
+     */
+    @Test
+    public void testMatrixSubtract5() {
+
+        BuildMatrix n = new BuildMatrix("[[1,0],[1,0],[1,0]".split(""));
+        Matrix one = n.createMatrix();
+        BuildMatrix v = new BuildMatrix("[[0,1],[0,1],[0,1]".split(""));
+        Matrix two = v.createMatrix();
+        MatrixSubtract sub = new MatrixSubtract();
+        System.out.println(sub.basicMatrixCompute(one, two));
+        assertEquals(sub.basicMatrixCompute(one, two).toString(), "[[1/1, -2/1], [1/1, -2/1], [1/1, -2/1]]");
+    }
 }
+
